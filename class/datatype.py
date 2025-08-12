9fad08f7c35fe7f4b77dfe07c659e86626312e78
Vansh2250 <vanshnpatel247@gmail.com>
2025-08-12 12:26:51 +0530

    all

---
+++ class/datatype.py
@@ -0,0 +1,29 @@
+#numeric
+
+a=10
+b=10.5
+c=4j+5
+
+print(type(a))
+print(type(b))
+print(type(c))
+
+name = "Tisha"
+subject = ["python","java","php"]
+t=("a","b","c",1,2,3)
+#range
+
+print(type(name))
+print(type(subject))
+print(type(t))
+
+s={"test","tech","a","b"}
+m={"python":"1","java":"2"}
+b=True
+
+print(type(s))
+print(type(m))
+print(type(b))
+
+a= input("enter number:")
+print(a)
--- class/demo.py
+++ class/demo.py
@@ -1,1 +1,15 @@
 print("Hello World")
+
+print(' Hello" python')
+print("hello ' python")
+print("""hello
+      python
+      hello
+      tops
+      """)
+
+print("hello","java","hello","python",sep=" | ")
+
+print('hello \" python')
+
+print("hello",end=" ")
