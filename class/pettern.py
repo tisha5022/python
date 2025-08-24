8c2b4dbd31d585246a5e4ca38f16373de638e9dd
Vansh2250 <vanshnpatel247@gmail.com>
2025-08-24 17:40:30 +0530

    all

--- class/pettern.py
+++ class/pettern.py
@@ -53,5 +53,43 @@
 #  ****
 # *****
 
-for i in range(1,6):
-    print(' '*(5-i)+"*"*i)
+# for i in range(1,6):
+#     print(' '*(5-i)+"*"*i)
+
+#    *
+#   * *
+#  * * *
+# * * * *
+#* * * * * 
+
+# for i in range(5):
+#     print(' '*(5-i)+"* "*(i+1))
+
+# * * * * * 
+#  * * * *
+#   * * * 
+#    * *
+#     *
+
+# for i in range(5,0,-1):
+#     print(" "*(5-i)+"* "*(i))
+    
+#    *
+#   *  * 
+#  *    *
+#   *  *
+#     *
+
+# for i in range(5):
+#     print(" ")
+
+# 1
+# 12
+# 123
+# 1234
+# 12345
+
+for i in range(0,5):
+    for j in range(i+1):
+        print((j+1) ,end=" ")
+    print()
