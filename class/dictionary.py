cf731ae796713ce420648411984c1aab8310c1b5
Vansh2250 <vanshnpatel247@gmail.com>
2025-09-11 16:17:12 +0530

    all

---
+++ class/dictionary.py
@@ -0,0 +1,19 @@
+st = {
+    "name":"Tisha",
+    "email":"tisha@gmail.com",
+    "language":["gujarati","hindi","english","maths"]
+}
+
+# print(st)
+
+# print(st['name'])
+
+# print(st.get('name'))
+
+# st['name'] = "Zeel"
+# print(st)
+
+# st.update({"subject":"Python","stram":"IT"})
+# print(st)
+
+print(st['language'][0])
