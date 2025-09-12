4c7b47a680df0d9944e0db0c862312eae556c2de
Vansh2250 <vanshnpatel247@gmail.com>
2025-09-12 09:16:48 +0530

    all

---
+++ assignment/module-7/lab.py
@@ -0,0 +1,4 @@
+#=============================lab 1===================================================
+
+# Q-1 Write a Python program to create a list with elements of multiple data types (integers, strings, floats,
+#     etc.).
